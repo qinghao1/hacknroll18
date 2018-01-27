83671f9395ec8efb798040eb0826934ff51d4096
derrick <derrick.2a4@gmail.com>
2018-01-28 01:23:46 +0800

    remove nonsense

--- app/models/round.rb
+++
@@ -1,6 +0,0 @@
-class Round
-  include Mongoid::Document
-  field :scores, type: Hash
-  field :finished, type: Boolean, default: false
-  embedded_in :game
-end
--- config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  get '/', to: 'sessions#index'
-  get '/:id', to: 'sessions#show'
-  post '/', to: 'sessions#create'
-  post '/:id', to: 'sessions#join'
-  get '/games/:id', to: 'games#show'
-  post '/games/:id', to: 'games#submit'
-end
