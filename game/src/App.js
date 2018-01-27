d2ea12fb596ebfd3b9c6d973ad0a6a61ae9d67a6
derrick <derrick.2a4@gmail.com>
2018-01-27 22:42:20 +0800

    okkk

--- game/src/App.js
+++ game/src/App.js
@@ -1,20 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {axios} from 'axios';
 
-class App extends Component {
+export default class App extends Component{
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title" >Face Off</h1>
-        </header>
-        <p className="App-intro">
-          bb don cri, is only gaem
-        </p>
+      <header className="App-header">
+      <h1 className="App-title" ><center>Face Off</center></h1>
+      </header>
+      <p className="App-intro"><center>
+      bb don cri, is only gaem
+      </center></p>
+      <p><button type="button" onClick={
+    axios.post('/user/12345', {
+        firstName: 'Fred',
+        lastName: 'Flintstone'
+      }
+      }><center>Create</center></button> </p>
+      <p><button type="button"><center>Join</center></button> </p>
       </div>
-    );
-  }
+    )}
 }
-
-export default App;
