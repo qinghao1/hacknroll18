55f8413d71b69469f09f468edc5a88b043313083
alexanderleegs <alexleegs@gmail.com>
2018-01-27 21:33:49 +0800

    game html test

---
+++ app/assets/javascripts/components/game.js.jsx
@@ -0,0 +1,14 @@
+var Game = createReactClass({
+
+  render: function() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title" >Face Off</h1>
+        </header>
+        <p className="App-intro">
+          bb don cri, is only gaem
+        </p>
+      </div>
+  	)}
+});
