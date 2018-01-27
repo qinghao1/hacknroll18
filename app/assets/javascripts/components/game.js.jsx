67d2784b4331519e4708b5f179710e42f6fde71b
alexanderleegs <alexleegs@gmail.com>
2018-01-27 21:47:53 +0800

    buttons

--- app/assets/javascripts/components/game.js.jsx
+++ app/assets/javascripts/components/game.js.jsx
@@ -4,11 +4,13 @@ var Game = createReactClass({
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title" >Face Off</h1>
+          <h1 className="App-title" ><center>Face Off</center></h1>
         </header>
-        <p className="App-intro">
+        <p className="App-intro"><center>
           bb don cri, is only gaem
-        </p>
+        </center></p>
+        <p><button type="button"><center>Create</center></button> </p>
+        <p><button type="button"><center>Join</center></button> </p>
       </div>
   	)}
 });
