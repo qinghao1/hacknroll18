082d856cdebd17602afa229cd36346645e30eff9
Chu Qing Hao <633703+qinghao1@users.noreply.github.com>
2018-01-27 18:21:31 +0800

    push

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,12 @@
+class GamesController < ApplicationController
+
+  # GET /game/:id
+  def show
+    @game = Game.where(id: params[:id])
+  end
+
+  # POST /game/:id
+  def submit
+    @game = Game.where(id: params[:id])
+  end
+end
